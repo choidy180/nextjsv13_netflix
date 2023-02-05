3bf88b87655e25ff43c450ad5aa8755d0ee5e65c
choidy180 <choidy180@naver.com>
2023-02-05 13:10:52 +0900

    'commit'

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,9 +34,9 @@ const Home = ({
 }:Props) => {
     const { loading } = useAuth()
     const showModal = useRecoilValue(modalState)
-    // const [showModal, setShowModal] = React.useState(false)
+    const subscription = false
 
-    if( loading) return null
+    if( loading || subscription === null) return null
     return (
         <div className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
             showModal && '!h-screen overflow-hidden'
